3d32201851676ae9548a7cff7cedd6d8aea29516
JYOTIPM1999 <jyotipm1999@gmail.com>
2022-10-24 12:09:28 +0530

    Products page started

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import "./App.css";
+import ProductPage from "./components/Jyoti/ProductPage/ProductPage";
+import SortingTv from "./components/Jyoti/SortingPage/SortingTv";
 
 function App() {
-  return <Box></Box>;
+  return (
+    <Box display="flex" justifyContent="space-between">
+      <SortingTv />
+      <ProductPage />
+    </Box>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Jyoti/ProductPage/ProductPage.jsx
@@ -0,0 +1,44 @@
+import { Box, Flex, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import React from "react";
+import { AiOutlineStar } from "react-icons/ai";
+
+function ProductPage() {
+  return (
+    <Box border="1px solid blue" p="2" width="80%">
+      <HStack>
+        <Box>
+          <Image
+            src="https://rukminim1.flixcart.com/image/312/312/kzn17680/television/o/q/p/a-3210s-f-adsun-original-imagbhqzhafpzhyz.jpeg?q=70"
+            w="200px"
+          />
+        </Box>
+        <VStack>
+          <Text>
+            Adsun Frameless 80 cm (32 inch) HD Ready LED Smart Android Based TV
+          </Text>
+          <HStack>
+            <Flex alignItems="center">
+              3.8 <AiOutlineStar />
+            </Flex>
+            <Text>10000 Ratings &</Text>
+            <Text>2500 Reviews</Text>
+          </HStack>
+          <Text>-operating System-Android based</Text>
+          <Text>-HD Ready 1366 x 768 Pixels</Text>
+          <Text>-3 years standard manufacturing warranty form admin</Text>
+        </VStack>
+        <VStack>
+          <Text>@ 8900</Text>
+          <HStack>
+            <Text as="s">29900</Text>
+            <Text>70% off</Text>
+          </HStack>
+          <Text>Free delivery</Text>
+          <Text>Bank Offer</Text>
+        </VStack>
+      </HStack>
+    </Box>
+  );
+}
+
+export default ProductPage;
