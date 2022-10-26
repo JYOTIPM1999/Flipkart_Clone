c20d920eac6c11ef1a7681233c360e9a5c391d4c
JYOTIPM1999 <jyotipm1999@gmail.com>
2022-10-26 10:20:46 +0530

    Folder structure updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import "./App.css";
+import ProductPage from "./components/Jyoti/ProductPage/ProductPage";
+import SortingTv from "./components/Jyoti/SortingPage/SortingTv";
+import Details from "./components/Utkarsh/Details";
 
 function App() {
   return <Box></Box>;
---
+++ frontend/src/components/Jyoti/SortingPage/SortingTv.jsx
@@ -0,0 +1,57 @@
+import { Box, Text, VStack } from "@chakra-ui/react";
+import React from "react";
+import RangeSlider from "../Slider/Slider";
+
+function SortingTv() {
+  return (
+    <Box border="1px solid red" p="2" width="100%" bg="white">
+      <VStack align="left">
+        <Box>
+          <Text>Filter</Text>
+          {/* <RangeSlider /> */}
+        </Box>
+        <Box>
+          <Text>Category</Text>
+        </Box>
+        <Box>
+          <Text>PRICE</Text>
+        </Box>
+        <Box>
+          <Text>BRAND</Text>
+        </Box>
+        <Box>
+          <Text>SCREEN SIZE</Text>
+        </Box>
+        <Box>
+          <Text>IDEAL FOR VIEWING DISTANCE</Text>
+        </Box>
+        <Box>
+          <Text>RESOLUTION</Text>
+        </Box>
+        <Box>
+          <Text>SCREEN TYPE</Text>
+        </Box>
+        <Box>
+          <Text>SMART TV</Text>
+        </Box>
+        <Box>
+          <Text>OPERATING SYSTEM</Text>
+        </Box>
+        <Box>
+          <Text>CONNECTIVITY FEATURE</Text>
+        </Box>
+        <Box>
+          <Text>CUSTOMER RATINGS</Text>
+        </Box>
+        <Box>
+          <Text>OFFERS</Text>
+        </Box>
+        <Box>
+          <Text>THEME</Text>
+        </Box>
+      </VStack>
+    </Box>
+  );
+}
+
+export default SortingTv;
