e9b98f388977d1ba3593a9d18f609a8aac2422ac
JYOTIPM1999 <jyotipm1999@gmail.com>
2022-11-18 00:28:23 +0530

    backend

--- backend/src/Schema/user.model.js
+++ backend/src/Schema/user.model.js
@@ -1,14 +1,9 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name:  {type:String,required:true},
-  email:  {type:String,required:true, unique:true},
-  password: {type:String,required:true},
-  // role: {
-  //   type: String,
-  //   enum: ["EXECUTIVE", "USER"],
-  //   default: "USER",
-  // },
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
 
 const userModel = mongoose.model("user", userSchema);
