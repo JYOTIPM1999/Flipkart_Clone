20e462599ce78f443d78d34174242dc0259937f8
utkarshqw <utkarshup75@gmail.com>
2022-11-18 01:21:29 +0530

    signin signup

--- backend/src/Routes/user.router.js
+++ backend/src/Routes/user.router.js
@@ -1,5 +1,61 @@
 const express = require("express");
 const UserModel = require("../Schema/user.model");
 const jwt = require("jsonwebtoken");
-
+const bcrypt = require("bcrypt")
 const app = express.Router();
+
+
+app.post("/signup",async(req,res)=>{
+
+    const {email,name,password} = req.body
+
+    const existingemail = await UserModel.findOne({email})
+
+    if(existingemail)
+    {
+        return res.send("User already exist")
+    }
+    
+     bcrypt.hash(password, 5, async function(err, hash) {
+         
+        let user = new UserModel({email,name,password:hash})
+        await user.save()
+        res.send("success")
+    });
+      
+
+})
+
+
+app.post("/signin",async(req,res)=>{
+
+    const {email,password} = req.body
+
+    var x = await UserModel.findOne({email})
+
+   if(!x)
+   {
+     return res.send("user not found")
+   }
+
+    bcrypt.compare(password, x.password, function(err, result) {
+        if(result)
+        {
+            const token = jwt.sign({id:x._id},"flipkartsecret")
+           
+           res.send({email:x.email,name:x.name,token})
+        }
+        else
+        {
+            res.send("wrong credentials")
+        }
+        
+    });
+
+
+
+
+})
+
+
+module.exports = app
