b9db4309c110f061f977818691f718aacf608da3
JYOTIPM1999 <jyotipm1999@gmail.com>
2022-11-20 14:32:14 +0530

    Schema

--- backend/src/Schema/tv.model.js
+++ backend/src/Schema/tv.model.js
@@ -1,12 +1,30 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  id: { type: Number },
-  name: { type: String },
-  rating: { type: Number },
-  price: { type: Number },
-  imglink: { type: String },
-  wish: { type: Boolean },
+  name: { type: String, required: true },
+  display_img: { type: String, required: true },
+  images: [
+    {
+      link: { type: String, required: true },
+      type: { type: String, required: true },
+    },
+  ],
+  brand: { type: String, required: true },
+  stars: { type: Number, required: true },
+  price: { type: Number, required: true },
+  previous_price: { type: Number, required: true },
+  discount_amount: { type: Number, required: true },
+  discount_percent: { type: Number, required: true },
+  stars_count: { type: [Number], required: true },
+  circular_rating: { type: [Number], required: true },
+  ratings: { type: Number, required: true },
+  reviews: { type: Number, required: true },
+  display_size: { type: [Number], required: true },
+  resolutions: { type: [String], required: true },
+  highlights: { type: [String], required: true },
+  general_specification: [{ head: String, info: String }],
+  ratingsreview: { type: [Number], required: true },
+  review_image: [{ type: [String] }],
 });
 
 const TvModel = mongoose.model("Tv", userSchema);
